586432f3f1a29d1bae6efc7f6dc395a6cda7f160
lauuurars <1120098177@u.icesi.edu.co>
2025-03-29 12:36:54 -0500

    fin actividad 3

---
+++ Clase Actividad 3/act3.js
@@ -0,0 +1,7 @@
+//Actividad 3
+
+let nombres = ["Pedro", "Carlos", "Ana", "Liliana", "Zara"] //agregamos nombres aleatorios
+console.log(nombres) //imprimimos el array original
+
+let ordenados = nombres.sort() //ordenamos el array alfabéticamente usando el método sort
+console.log(ordenados) //imprimimos el array ordenado alfabéticamente
