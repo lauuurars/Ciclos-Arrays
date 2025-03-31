c7b4d8385769fc8f2fbf9ce9a2a383dde13d532e
lauuurars <1120098177@u.icesi.edu.co>
2025-03-31 08:54:42 -0500

    fin punto 3

---
+++ Act. 3 Ciclos/ciclos3.js
@@ -0,0 +1,16 @@
+//Actividad 3 -- For
+
+let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for (let i=0; i<9; i++) {
+    console.log("Tabla de multiplicar del número: ", numeros)
+    console.log(numeros[i] * 1)
+    console.log(numeros[i] * 2)
+    console.log(numeros[i] * 3)
+    console.log(numeros[i] * 4)
+    console.log(numeros[i] * 5)
+    console.log(numeros[i] * 6)
+    console.log(numeros[i] * 7)
+    console.log(numeros[i] * 8)
+    console.log(numeros[i] * 9)
+}
