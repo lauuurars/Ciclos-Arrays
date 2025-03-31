9bb1998b5c31d493442d1e2cae775f10ff74b5e3
lauuurars <1120098177@u.icesi.edu.co>
2025-03-31 08:26:21 -0500

    fin punto 10

---
+++ Act. 10 Ciclos/ciclos10.js
@@ -0,0 +1,12 @@
+//Actividad 10 
+
+let deseaSalir
+
+do{
+    let monto = prompt("Ingrese el monto de la moneda")
+
+    console.log("El monto de la moneda es", monto)
+
+    deseaSalir = prompt("Desea salir? Ingrese 'Si' o 'No'")
+
+} while (deseaSalir == 'no')
