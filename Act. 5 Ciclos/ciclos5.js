d1b0df0f286fc8e3431cc4da6ecedb04e20523ad
lauuurars <1120098177@u.icesi.edu.co>
2025-03-31 09:25:19 -0500

    fin actividad 5

---
+++ Act. 5 Ciclos/ciclos5.js
@@ -0,0 +1,10 @@
+//Actividad 5
+
+let palabras = ["Esternocleidomastoideo", "Fresa", "Miau", "Kuromi"]
+
+for (const palabra of palabras) {
+    console.log("Palabras: " + palabra)
+    let longitud = palabra.length
+    console.log("Longitud: " + longitud)
+}
+console.log("Estoy fuera!")
